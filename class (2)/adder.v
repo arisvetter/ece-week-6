73c52e06c120214cf9a1d16a3e75a61d25e1402f
arisvetter <142842842+arisvetter@users.noreply.github.com>
2025-09-30 15:53:57 +0000

    draft

--- class (2)/adder.v
+++ class (2)/adder.v
@@ -1,9 +1,14 @@
 module adder(
     // Declare your A/B inputs
+    input A, B
     // Declare Y output
+    output Y
     // Declare carry output
+    output Carry
 );
 
     // Enter logic equation here
+    Y = A ^ B;
+    Carry = A & B;
 
 endmodule
